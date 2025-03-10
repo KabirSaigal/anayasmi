c85e0d7252446662e67813578c6f2a1f2271adb0
Kabir Saigal <kabirsaigal9@gmail.com>
2025-03-11 00:14:09 +0530

    Init

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Product
+    {
+      id: number;
+      name: string;
+      price: number;
+    };
+
+let ccount: number = 0;
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +16,63 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
+
 export class DashboardComponent {
+  constructor(private router: Router) {
+  }
+
+  ccount = ccount;
+
+  ngOnInit(): void {
+    this.updateCartCount();
+  }
+
+  logout(): void {
+    this.router.navigate(['/login']);
+  }
+
+  products: Product[] = [
+    {id: 1,
+      name: 'Grand Theft Auto V',
+      price: 99.99
+    },
+    {id: 2,
+      name: 'Spiderman 2',
+      price: 39.99
+    },
+    {id: 3,
+      name: 'Black Ops 6',
+      price: 39.99
+    }
+  ];
+
+  cart: Product[] = [];
+
+  addToCart(item:Product): void {
+    console.log("Adding to cart");
+    this.cart.push(item);
+    this.updateCartCount();
+  }
+
+  removeFromCart(item:Product): void {
+    this.cart = this.cart.filter(i => i !== item);
+    this.updateCartCount();
+  }
+
+  total(): number {
+    return this.cart.reduce((acc, item) => acc + item.price, 0);
+  }
+
+  getProduct(id: number): Product {
+    const product = this.products.find(p => p.id === id);
+    if (!product) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  updateCartCount(): void {
+    this.ccount = this.cart.length;
+  }
 
 }
